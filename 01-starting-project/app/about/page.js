dd1b1a2125f8a292fa07ee348d2a561f1d2623bd
sloyardms <sloyard_ms@outlook.com>
2025-08-23 17:32:38 -0700

    lesson-89: Adding Another Route via the File System

---
+++ 01-starting-project/app/about/page.js
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+        <main>
+            <div>
+                <h1>About Us</h1>
+                <p>This is the about page of our application.</p>
+            </div>
+        </main>
+    );
+}
