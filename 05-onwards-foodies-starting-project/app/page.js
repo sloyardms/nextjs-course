ed0193bc965d66ae54de6fc4afa7ca5eff5c52e4
sloyardms <sloyard_ms@outlook.com>
2025-08-26 14:29:07 -0700

    lesson-104: Preparing an Image Slideshow

--- 05-onwards-foodies-starting-project/app/page.js
+++ 05-onwards-foodies-starting-project/app/page.js
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
