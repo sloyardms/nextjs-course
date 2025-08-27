21cb7c1c52ebfbf94fabc6ad413899062ddb5331
sloyardms <sloyard_ms@outlook.com>
2025-08-27 02:14:27 -0700

    lesson-133: Adding Dynamic Metadata

--- 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
+++ 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -3,15 +3,28 @@ import classes from './page.module.css'
 import { getMeal } from '@/lib/meals'
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({ params }) {
+    const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+    
+    return {
+        title: meal.title,
+        description: meal.summary,
+    };
+}
+
 export default function MealDetailsPage({ params }) {
     const meal = getMeal(params.mealSlug);
-    
-    if(!meal){
+
+    if (!meal) {
         notFound();
     }
 
     meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
-    
+
     return <>
         <header className={classes.header}>
             <div className={classes.image}>
