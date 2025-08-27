8f52a44a62eaaa82deae2f2b618d8c5999c2ad9c
sloyardms <sloyard_ms@outlook.com>
2025-08-26 17:56:18 -0700

    lesson-114: Loading & Rendering Meal Details via Dynamic Routes

--- 05-onwards-foodies-starting-project/lib/meals.js
+++ 05-onwards-foodies-starting-project/lib/meals.js
@@ -3,9 +3,14 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals(){
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    //await new Promise((resolve) => setTimeout(resolve, 2000));
 
     //throw  new Error("Loading meals failed");
     const meals = db.prepare("SELECT * FROM meals").all();
     return meals;
+}
+
+export function getMeal(slug){
+    const meal = db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+    return meal;
 }
