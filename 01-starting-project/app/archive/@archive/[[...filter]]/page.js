d5e4373029fb1796abb2ebbf05522ba19de36a9f
sloyardms <sloyard_ms@outlook.com>
2025-09-04 16:25:35 -0700

    lesson-144: Catch-All Fallback Routes & dealing with Multiple Path Segments

--- 01-starting-project/app/archive/@archive/[[...filter]]/page.js
+++ 01-starting-project/app/archive/@archive/[[...filter]]/page.js
@@ -1,25 +1,51 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 import Link from "next/link";
 
 export default function FilteredNewsPage({ params }) {
     const filter = params.filter;
-    //const news = getNewsForYear(newsYear);
 
-    const links = getAvailableNewsYears();
+    const selectedYear = filter ? filter[0] : undefined;
+    const selectedMonth = filter ? filter[1] : undefined;
+
+    let news;
+    let links = getAvailableNewsYears();
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
+    }
+
+    if(selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = [];
+    }
+
+    let newsContent = <p>No news found for the selected period.</p>
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />
+    }
 
     return (
-        <header id="archive-header">
-            <nav>
-                <ul>
-                    {links.map((link) => (
-                        <li key={link}>
-                            <Link href={`/archive/${link}`}>{link}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header id="archive-header">
+                <nav>
+                    <ul>
+                        {links.map((link) => {
+                            const href = selectedYear 
+                                ? `/archive/${selectedYear}/${link}` 
+                                : `/archive/${link}`;
+                            return (
+                                <li key={link}>
+                                    <Link href={href}>{link}</Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            </header>
+            {newsContent}
+        </>
     );
-    //return <NewsList news={news} />
 }
