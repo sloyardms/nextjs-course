abc1cc51a471d800b3a3b976c9d2ad28fee06311
sloyardms <sloyard_ms@outlook.com>
2025-09-04 19:19:40 -0700

    lesson-152: Navigating Programmatically

--- 01-starting-project/app/news/[slug]/@modal/(.)image/page.js
+++ 01-starting-project/app/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,10 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+    const router = useRouter();
     const newsSlug = params.slug;
     const newsItem = DUMMY_NEWS.find(news => news.slug === newsSlug);
 
@@ -10,7 +13,7 @@ export default function InterceptedImagePage({ params }) {
     }
 
     return <>
-        <div className="modal-backdrop"></div>
+        <div className="modal-backdrop" onClick={router.back}></div>
         <dialog className="modal" open>
             <div className="fullscreen-image">
                 <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
