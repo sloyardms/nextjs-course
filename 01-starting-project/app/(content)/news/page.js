631dd08e5b9f8a954054c843bcab203630680561
sloyardms <sloyard_ms@outlook.com>
2025-09-06 16:03:15 -0700

    lesson-163: Showing a Loading Fallback

--- 01-starting-project/app/(content)/news/page.js
+++ 01-starting-project/app/(content)/news/page.js
@@ -1,14 +1,9 @@
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 
 export default async function NewsPage() {
-    const response = await fetch("http://localhost:8080/news");
-
-    if (!response.ok) {
-        throw new Error("Failed to fetch news");
-    }
-
-    const news = await response.json();
-
+    const news = await getAllNews();
+    
     return <>
         <h1>The News Page</h1>
         <NewsList news={news} />
