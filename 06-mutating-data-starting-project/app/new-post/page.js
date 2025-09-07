8e935be5de22ad24ee437063e5efd59a95fce75f
sloyardms <sloyard_ms@outlook.com>
2025-09-06 19:02:09 -0700

    lesson-173: Validating User Input With Help Of The useFormState Hook

--- 06-mutating-data-starting-project/app/new-post/page.js
+++ 06-mutating-data-starting-project/app/new-post/page.js
@@ -1,6 +1,9 @@
+"use client";
+
 import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
+import { useActionState } from "react";
 
 export default function NewPostPage() {
 
@@ -9,6 +12,24 @@ export default function NewPostPage() {
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
+
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required");
+    }
+
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required");
+    }
+
+    if(!image || image.size === 0) {
+      errors.push("Image is required");
+    }
+
+    if(errors.length > 0) {
+      return {errors};
+    }
 
     await storePost({
       imageUrl: "",
@@ -20,10 +41,12 @@ export default function NewPostPage() {
     redirect("/feed");
   }
 
+  const [state, formAction] = useActionState(createPost, {});
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
