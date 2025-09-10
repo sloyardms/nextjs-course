48faca594d7f500c600267f66a8c1dd0817863d5
sloyardms <sloyard_ms@outlook.com>
2025-09-10 16:00:12 -0700

    lesson-199: Loading Unknown Images

--- 08-app-optimizations-starting-project/components/posts.js
+++ 08-app-optimizations-starting-project/components/posts.js
@@ -5,12 +5,17 @@ import { useOptimistic } from 'react';
 import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
 import { togglePostLikeStatus } from '@/actions/posts';
+import Image from 'next/image';
 
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <img src={post.image} alt={post.title} />
+        <Image 
+          src={post.image} 
+          alt={post.title}
+          fill 
+          />
       </div>
       <div className="post-content">
         <header>
