1a9aa902a3dcb385fd4765de600c2ad56b22d56a
sloyardms <sloyard_ms@outlook.com>
2025-09-10 16:39:27 -0700

    lesson-201: Using An Image Loader & Cloudinary Resizing

--- 08-app-optimizations-starting-project/components/posts.js
+++ 08-app-optimizations-starting-project/components/posts.js
@@ -7,15 +7,27 @@ import LikeButton from './like-icon';
 import { togglePostLikeStatus } from '@/actions/posts';
 import Image from 'next/image';
 
+function imageLoader(config) {
+  const urlStart = config.src.split('upload/')[0];
+  const urlEnd = config.src.split('upload/')[1];
+  const transformations = `w_500,q_${config.quality || 75}`;
+  const finalUrl = `${urlStart}upload/${transformations}/${urlEnd}`;
+
+  return finalUrl;
+}
+
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <Image 
-          src={post.image} 
+        <Image
+          loader={imageLoader}
+          src={post.image}
           alt={post.title}
-          fill 
-          />
+          quality={75}
+          sizes="8rem"
+          fill
+        />
       </div>
       <div className="post-content">
         <header>
