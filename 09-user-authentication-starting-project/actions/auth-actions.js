d039c2f60eec36df61616e25be011fca5d66e281
sloyardms <sloyard_ms@outlook.com>
2025-09-15 16:09:40 -0700

    lesson-216: Setting Up An Auth Session

--- 09-user-authentication-starting-project/actions/auth-actions.js
+++ 09-user-authentication-starting-project/actions/auth-actions.js
@@ -1,5 +1,6 @@
 "use server"
 
+import { createAuthSession } from "@/lib/auth";
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
 import { redirect } from "next/navigation";
@@ -27,7 +28,9 @@ export async function signup(prevState, formData) {
     //store it in the database (create new user)
     const hashedPassword = hashUserPassword(password);
     try {
-        createUser(email, hashedPassword);
+        const id = createUser(email, hashedPassword);
+        await createAuthSession(id)
+        redirect("/training");
     } catch (error) {
         if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
             return {
@@ -38,6 +41,4 @@ export async function signup(prevState, formData) {
         }
         throw error;
     }
-
-    redirect("/training");
 }
--- 09-user-authentication-starting-project/lib/auth.js
+++ 09-user-authentication-starting-project/lib/auth.js
@@ -12,7 +12,7 @@ const lucia = new Lucia(adapter, {
     sessionCookie: {
         expires: false,
         attributes: {
-            secure: process.env.NODE_ENV = "production"
+            secure: process.env.NODE_ENV === "production"
         }
     }
 });
