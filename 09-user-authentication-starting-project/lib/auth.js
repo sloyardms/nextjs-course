c11eec9d97930903bcbb291688e9511af3229c8b
sloyardms <sloyard_ms@outlook.com>
2025-09-15 16:14:54 -0700

    lesson-217: Verifying An Active Auth Session

--- 09-user-authentication-starting-project/lib/auth.js
+++ 09-user-authentication-starting-project/lib/auth.js
@@ -26,3 +26,45 @@ export async function createAuthSession(userId) {
         sessionCookie.attributes
     );
 }
+
+export async function verifyAuth() {
+    const sessionCookie = (await cookies()).get(lucia.sessionCookieName);
+    if (!sessionCookie) {
+        return {
+            user: null,
+            session: null,
+        }
+    }
+
+    const sessionId = sessionCookie.value;
+    if (!sessionId) {
+        return {
+            user: null,
+            session: null,
+        }
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+        if (result.session && result.session.fresh) {
+            const sessionCookie = lucia.createSessionCookie(result.session.id);
+            (await cookies()).set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            );
+        }
+
+        if (!result.session) {
+            const sessionCookie = lucia.createBlankSessionCookie();
+            (await cookies()).set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            );
+        }
+    } catch { }
+
+    return result;
+}
