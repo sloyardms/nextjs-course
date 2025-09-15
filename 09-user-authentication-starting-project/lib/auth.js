3542a95389c7446c307f7940f5dfc18ffa7625ea
sloyardms <sloyard_ms@outlook.com>
2025-09-15 16:04:30 -0700

    lesson-215: Configuring  A Session & A Session Cookie

--- 09-user-authentication-starting-project/lib/auth.js
+++ 09-user-authentication-starting-project/lib/auth.js
@@ -1,6 +1,7 @@
-import {Lucia} from "lucia";
+import { Lucia } from "lucia";
 import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
 import db from "./db";
+import { cookies } from "next/headers";
 
 const adapter = new BetterSqlite3Adapter(db, {
     user: "users",
@@ -15,3 +16,13 @@ const lucia = new Lucia(adapter, {
         }
     }
 });
+
+export async function createAuthSession(userId) {
+    const session = await lucia.createSession(userId, {})
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    (await cookies()).set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+    );
+}
